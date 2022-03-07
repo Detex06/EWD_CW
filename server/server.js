cb2d450b08df7c72c746a83901553c68de8ad36c
Detex06 <a.kaminski2@rgu.ac.uk>
2022-03-07 19:56:03 +0000

    Part 2/3

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import config from './../config/config'
+import app from './express'
+import mongoose from 'mongoose'
+
+//might need to fix later with the "mongodb+srv://<myusername>:<mypassword>@cluster0.dgpco.mongodb.net/<mydatabasename>?retryWrites=true&w=majority"
+const mongoUri = process.env.URI ||  "mongodb://127.0.0.1:27017";
+
+app.listen(config.port, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on port %s.', config.port)
+})
+
+// Connection URL
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { dbName: "users" })
+mongoose.connection.on('error', err => {
+ throw new Error(`unable to connect to database: ${config.mongoUri}`)
+})
