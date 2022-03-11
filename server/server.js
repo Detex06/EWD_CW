acef7c9039a5da861ff6b88da0ce6258dbe7eb29
Detex06 <a.kaminski2@rgu.ac.uk>
2022-03-11 16:11:45 +0000

    mongoose code first and also use MONGODB_URI now

--- server/server.js
+++ server/server.js
@@ -3,7 +3,16 @@ import app from './express'
 import mongoose from 'mongoose'
 
 //might need to fix later with the "mongodb+srv://<myusername>:<mypassword>@cluster0.dgpco.mongodb.net/<mydatabasename>?retryWrites=true&w=majority"
-const mongoUri = process.env.URI ||  "mongodb://127.0.0.1:27017";
+//const mongoUri = process.env.URI ||  "mongodb://127.0.0.1:27017";
+
+// Connection URL
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { dbName: "users" })
+mongoose.connection.on('error', err => {
+ throw new Error(`unable to connect to database: ${config.mongoUri}`)
+})
+
+
 
 app.listen(config.port, (err) => {
     if (err) {
@@ -12,16 +21,4 @@ app.listen(config.port, (err) => {
     console.info('Server started on port %s.', config.port)
 })
 
-// Connection URL
-try {
-mongoose.Promise = global.Promise
-await mongoose.connect(config.mongoUri, { dbName: "users" })
-}
-catch (error) {
-    handleError(error);
-}
-/*
-mongoose.connection.on('error', err => {
- throw new Error(`unable to connect to database: ${config.mongoUri}`)
-})
-*/
+
