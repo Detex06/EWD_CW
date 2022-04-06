d3804bd1b31e6d8474f88885f7910e27691c8cb3
Alex Kaminski <alex.kaminski.00@gmail.com>
2022-04-06 17:12:50 +0100

    Update Home.js

--- client/core/Home.js
+++ client/core/Home.js
@@ -85,7 +85,7 @@ export default function Home() {
 */
 export default function Home() {
     const classes = useStyles()
-    const {items} = useState([]);
+    const {items} = products;
     useEffect(() => {
         const abortController = new AbortController()
         const signal = abortController.signal
