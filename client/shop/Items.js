9d6ff66a9f3d8f1a133343fb8dcdc15c59b995a5
Alex Kaminski <alex.kaminski.00@gmail.com>
2022-04-07 16:18:29 +0100

    Update Items.js

--- client/shop/Items.js
+++ client/shop/Items.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import ArrowForward from '@material-ui/icons/ArrowForward'
+import Person from '@material-ui/icons/Person'
 
 const Item = (prop) => {
     
