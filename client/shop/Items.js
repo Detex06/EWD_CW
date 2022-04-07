121b165864f954fefd155f96a5df18a70879f0be
Alex Kaminski <alex.kaminski.00@gmail.com>
2022-04-07 21:51:08 +0100

    reverting all changes to items.js

--- client/shop/Items.js
+++ client/shop/Items.js
@@ -9,43 +9,40 @@ import Avatar from '@material-ui/core/Avatar'
 import IconButton from '@material-ui/core/IconButton'
 import ArrowForward from '@material-ui/icons/ArrowForward'
 import Person from '@material-ui/icons/Person'
-import { Button } from 'bootstrap'
 
 const Items = (prop) => {
-
+    
     return (
         <List dense>
-            {prop.items.map((item) => {
-                console.log("LOADING ITEMS");
-                console.log(JSON.stringify(item));
-                if (item.amount !== 0) {
-                    return (
+                {prop.items.map((item) => {
+                    console.log("LOADING ITEMS");
+                    console.log(JSON.stringify(item));
+                    if (item.amount !== 0) {
+                        return (
+                            
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <Person />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary={item.name} secondary={"£" + item.price} />
 
-                        <ListItem>
-                            <ListItemAvatar>
-                                <Avatar>
-                                    <Person />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={item.name} secondary={"£" + item.price} />
-
-                            <ListItemSecondaryAction>
-                                <IconButton>
-                                    <Button>Add To basket</Button>
-                                </IconButton>
-                            </ListItemSecondaryAction>
-
-
-                        </ListItem>
-
-                    )
+                                <ListItemSecondaryAction>
+                                    <IconButton>
+                                        <ArrowForward />
+                                    </IconButton>
+                                </ListItemSecondaryAction>
+                                
+                                
+                            </ListItem>
+                            
+                        )
+                    }
+                })
                 }
-            })
-            }
-        </List>
+            </List>
     )
 }
-
-onAdd
 
 export default Items;
