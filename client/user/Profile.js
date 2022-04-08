e73e0983c65d0a445ce17a5dd9db9ee951fa9445
Alex Kaminski <alex.kaminski.00@gmail.com>
2022-04-08 17:06:02 +0100

    Update Profile.js

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -14,8 +14,8 @@ import Person from '@material-ui/icons/Person'
 import Divider from '@material-ui/core/Divider'
 import DeleteUser from './DeleteUser'
 import auth from './../auth/auth-helper'
-import {read} from './api-user.js'
-import {Redirect, Link} from 'react-router-dom'
+import { read } from './api-user.js'
+import { Redirect, Link } from 'react-router-dom'
 import Basket from '../shop/Basket'
 
 const useStyles = makeStyles(theme => ({
@@ -43,7 +43,7 @@ export default function Profile({ match }) {
 
     read({
       userId: match.params.userId
-    }, {t: jwt.token}, signal).then((data) => {
+    }, { t: jwt.token }, signal).then((data) => {
       if (data && data.error) {
         setRedirectToSignin(true)
       } else {
@@ -51,64 +51,61 @@ export default function Profile({ match }) {
       }
     })
 
-    return function cleanup(){
+    return function cleanup() {
       abortController.abort()
     }
 
   }, [match.params.userId])
-  
-    if (redirectToSignin) {
-      return <Redirect to='/signin'/>
-    }
-    
-    //console.log(JSON.stringify(user))
-    console.log(JSON.stringify(user.basket))
-    return (
-      <Paper className={classes.root} elevation={4}>
-        <Typography variant="h6" className={classes.title}>
-          Profile
-        </Typography>
-        <List dense>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar>
-                <Person/>
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={user.name} secondary={user.email}/> {
-             auth.isAuthenticated().user && auth.isAuthenticated().user._id == user._id &&
-              (<ListItemSecondaryAction>
-                <Link to={"/user/edit/" + user._id}>
-                  <IconButton aria-label="Edit" color="primary">
-                    <Edit/>
-                  </IconButton>
-                </Link>
-                <DeleteUser userId={user._id}/>
-              </ListItemSecondaryAction>)
-            }
-          </ListItem>
-          <ListItem>
-            <ListItemText primary={user.about}/>
-          </ListItem>
-          <Divider/>
-          <ListItem>
-            <Typography>
-              User Basket
-            </Typography>
-            <ListItemText primary={JSON.stringify(user.basket)}/>
-          </ListItem>
-          <Divider/>
-          <ListItem>
-            <ListItemText primary={"Joined: " + (
-              new Date(user.created)).toDateString()}/>
-          </ListItem>
-          <ListItem>
-            <ListItemText primary={"Profile edits:" + user.profileclicks}/>
-          </ListItem>
-        </List>
-              
-              
-        <Basket basket={user.basket}></Basket>
-      </Paper>
-    )
+
+  if (redirectToSignin) {
+    return <Redirect to='/signin' />
   }
+
+  //console.log(JSON.stringify(user))
+  console.log(JSON.stringify(user.basket))
+  return (
+    <Paper className={classes.root} elevation={4}>
+      <Typography variant="h6" className={classes.title}>
+        Profile
+      </Typography>
+      <List dense>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <Person />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={user.name} secondary={user.email} /> {
+            auth.isAuthenticated().user && auth.isAuthenticated().user._id == user._id &&
+            (<ListItemSecondaryAction>
+              <Link to={"/user/edit/" + user._id}>
+                <IconButton aria-label="Edit" color="primary">
+                  <Edit />
+                </IconButton>
+              </Link>
+              <DeleteUser userId={user._id} />
+            </ListItemSecondaryAction>)
+          }
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={user.about} />
+        </ListItem>
+        <Divider />
+        <ListItem>
+          <ListItemText primary={"Joined: " + (
+            new Date(user.created)).toDateString()} />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary={"Profile edits:" + user.profileclicks} />
+        </ListItem>
+      </List>
+
+      <Divider />
+
+      <Typography>
+        User Basket
+      </Typography>
+      <Basket basket={user.basket}></Basket>
+    </Paper>
+  )
+}
