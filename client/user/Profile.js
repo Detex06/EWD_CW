2425a610c761fbac7e3728b1ef079b3bc75c0293
Alex Kaminski <alex.kaminski.00@gmail.com>
2022-04-08 15:07:22 +0100

    Update Profile.js

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -94,7 +94,7 @@ export default function Profile({ match }) {
             <Typography>
               User Basket
             </Typography>
-            <ListItemText primary={JSON.stringify(user.basket)}/>
+            <ListItemText primary={JSON.stringify(user.basket.name)}/>
           </ListItem>
           <Divider/>
           <ListItem>
