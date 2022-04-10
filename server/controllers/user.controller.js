94b77d6304065ef6fd0a624fa2febf2bcaa88fe4
Alex Kaminski <alex.kaminski.00@gmail.com>
2022-04-10 01:02:20 +0100

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -134,7 +134,7 @@ const removeFromBasket = async (req, res) => {
     try {
         let user = req.profile
         user = extend(user, req.body)
-        await user.updateOne({ $pull: { basket: { $in: [basket] } } })
+        await user.updateOne({ $pull: { basket: { $in: [basket.name] } } })
         user.hashed_password = undefined
         user.salt = undefined
         res.json(user)
