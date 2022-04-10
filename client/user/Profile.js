7985cfeb7e1f29b19ba569302623999657c0b8a3
Alex Kaminski <alex.kaminski.00@gmail.com>
2022-04-10 01:27:30 +0100

    Update Profile.js

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -57,7 +57,7 @@ export default function Profile({ match }) {
 
   }, [match.params.userId])
 
-  if (values.redirectToProfile) {
+  if (redirectToProfile) {
     return (<Redirect to={'/user/' + user.userId} />)
   }
 
