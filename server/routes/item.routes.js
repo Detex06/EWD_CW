35881b52cb07fde56738e204a69d5c679818ab9f
Alex Kaminski <alex.kaminski.00@gmail.com>
2022-04-10 20:16:33 +0100

    Update item.routes.js

--- server/routes/item.routes.js
+++ server/routes/item.routes.js
@@ -4,9 +4,9 @@ import itemCtrl from '../controllers/item.controller'
 const router = express.Router()
 
 
-router.route('/api/')
-    .get(itemCtrl.listItems)
-    //.post(itemCtrl.create)
+// router.route('/api/')
+//     .get(itemCtrl.listItems)
+//     //.post(itemCtrl.create)
 
 
 export default router
